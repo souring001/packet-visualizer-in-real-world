3cc79bfd5c44be93793d77c0607ef5f89983d8a1
aso <titechei@gmail.com>
2019-08-09 20:36:49 +0900

    Add GPIO.cleanup()

--- lchika.py
+++ lchika.py
@@ -3,9 +3,10 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3, GPIO.OUT)
-while True:
-    GPIO.output(3, True)
+GPIO.setup(11, GPIO.OUT)
+for x in range(5):
+    GPIO.output(11, True)
     time.sleep(2)
-    GPIO.output(3, False)
+    GPIO.output(11, False)
     time.sleep(2)
+GPIO.cleanup()
